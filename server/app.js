f64bae659d93067670e8746ef2186f0062c1d554
Jisu00 <jisudev00@gmail.com>
2022-04-30 02:48:23 +0900

    Feat: Connect node.js & mySQL
    node.js와 mySQL 연동

--- server/app.js
+++ server/app.js
@@ -1,9 +1,23 @@
 const express = require('express');
-const app = express();
-const test = require('./Routes/test');
+require('dotenv').config({ path: 'config/.env' });
 
-app.use("/test", test);
+const mysql = require('./config/db.js');
+const app = express();
+
+const indexRouter = require('./Routes/index');
+const keywordRouter = require('./Routes/keyword');
+const reviewRouter = require('./Routes/review');
+
+app.use("/", indexRouter);
+app.use("/keyword", keywordRouter);
+app.use("/review", reviewRouter);
 
 const port = 4000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+app.get("/api/category", async(req, res) => {
+  const category = await mysql.query('category');
+  console.log(category);
+  res.send(category);
+})
---
+++ server/config/sql.js
@@ -0,0 +1,3 @@
+module.exports = {
+  category: `select * from category`
+}
